a090009364243025c4c5b12f58caf030f9a8cfde
Alexandr Spesivtsev <dart_sas@mail.ru>
2016-02-25 22:43:40 +0700

    start workin on tokens

---
+++ app/Models/Token.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: sasik
+ * Date: 2/25/16
+ * Time: 9:14 PM
+ */
+
+namespace App\Models;
+
+
+class Token extends BaseModel
+{
+
+}
---
+++ database/migrations/2016_02_25_141519_create_tokens_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('token');
+            $table->unsignedInteger('device_type_id');
+            $table->unsignedInteger('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tokens');
+    }
+}
