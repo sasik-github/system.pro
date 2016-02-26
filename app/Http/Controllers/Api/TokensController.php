fa98260b729f5694b5eb28e1f9db8037fa2c8309
Alexandr Spesivtsev <dart_sas@mail.ru>
2016-02-26 10:26:52 +0700

    save device tokens to a database

--- app/Http/Controllers/Api/TokensController.php
+++ app/Http/Controllers/Api/TokensController.php
@@ -28,8 +28,11 @@ class TokensController extends BaseController
      * @apiParam {String} token Уникальный token устройства из GCM или APNS
      * @apiParam {Int} device_type_id Тип устройства(ANDROID = 1, IOS = 2)
      *
-     *  @apiSuccessExample Success-Response:
+     * @apiSuccessExample Success-Response:
      *     HTTP/1.1 200 OK
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response|static
      */
     public function store(Request $request)
     {
@@ -37,6 +40,10 @@ class TokensController extends BaseController
          * @var $user User
          */
         $user = auth()->user();
+        if (!$user) {
+            return response('User doesn\'t exist', 401);
+        }
+
         $attributes = $request->all();
         $attributes['user_id'] = $user->id;
         $token = Token::create($attributes);
