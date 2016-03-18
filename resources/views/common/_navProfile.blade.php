0a1c730c16f6ef99af5ca76dbd92c12e49cc9c98
Alexandr Spesivtsev <dart_sas@mail.ru>
2016-03-18 17:22:42 +0700

    menu

--- resources/views/common/_navProfile.blade.php
+++ resources/views/common/_navProfile.blade.php
@@ -1,8 +1,8 @@
 <ul class="nav navbar-nav navbar-right">
     <!-- Authentication Links -->
     @if (Auth::guest())
-        <li><a href="{{ url('/login') }}">Login</a></li>
-        <li><a href="{{ url('/register') }}">Register</a></li>
+        <li><a href="{{ url('/login') }}">Войти</a></li>
+        <li><a href="{{ url('/register') }}">Регистраци</a></li>
     @else
 
         <li class="dropdown">
