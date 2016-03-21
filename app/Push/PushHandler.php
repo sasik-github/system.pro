a4e4fd534940396535ebe27b3e808e9a702d6cb2
Alexandr Spesivtsev <dart_sas@mail.ru>
2016-03-21 14:39:56 +0700

    added datetime to Push

--- app/Push/PushHandler.php
+++ app/Push/PushHandler.php
@@ -99,6 +99,7 @@ class PushHandler
             'child_id' => $event->child->id,
             'card_number' => $event->child->card_number,
             'type' => $event->event_type_id,
+            'datetime' => $event->created_at,
             'id' => $event->id,
         ];
     }
