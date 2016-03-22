797a49ee37515ee0cac3c99a9063602d07889a7f
Alexandr Spesivtsev <dart_sas@mail.ru>
2016-03-22 23:02:08 +0700

    added stats API

--- resources/views/parentProfile/_personalData.blade.php
+++ resources/views/parentProfile/_personalData.blade.php
@@ -2,8 +2,7 @@
     <div class="panel-heading">Мои данные</div>
     <div class="panel-body">
         <div>ФИО: {{ $parent->fio }}</div>
-        <div>Текущий тарифный план: @if (array_key_exists($parent->tariff_id, $tariffs))
-                {{ $tariffs[$parent->tariff_id] }}
-            @endif</div>
+        <div>Счет: {{ $parent->account }}</div>
+        <div>Текущий тарифный план: {{ $parent->tariffs[0] }}
     </div>
 </div>
--- resources/views/parentProfile/_tariffChooserForm.blade.php
+++ resources/views/parentProfile/_tariffChooserForm.blade.php
@@ -1,7 +1,12 @@
 {!! Form::open(['url' => 'profile/tariff-submission']) !!}
 
-    {!! Form::select('tariff_id', $tariffs, null, []) !!}
 
+    @foreach($tariffs as $id => $tariff)
+{{--        {!! var_dump([$id => $tariff]) !!}--}}
+        <label for="tariff_{{$id}}">
+            {!! Form::radio('tariff_id', $id, null, ['id' => 'tariff_' . $id]) !!} {{ $tariff }}
+        </label>
+    @endforeach
     {!! Form::submit('Выбрать') !!}
 
 {!! Form::close() !!}
