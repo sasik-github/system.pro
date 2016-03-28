fb9f9fa88a354393ceb4e558e1e087479facb9c6
Alexandr Spesivtsev <dart_sas@mail.ru>
2016-03-28 09:55:57 +0700

    added test event form for admin

--- resources/views/common/_navAdmin.blade.php
+++ resources/views/common/_navAdmin.blade.php
@@ -4,7 +4,7 @@
     <li class="{{ Ekko::isActiveMatch('parents') }}"><a href="{{ action('ParentsController@getIndex') }}">Родители</a></li>
     <li class="{{ Ekko::isActiveMatch('about') }}"><a href="{{ action('AboutController@getEdit') }}">Редактор "О Компании"</a></li>
     <li class="{{ Ekko::isActiveMatch('tariffs') }}"><a href="{{ action('TariffsController@getIndex') }}">Тарифы</a></li>
-    <li class="{{ Ekko::isActiveMatch('events') }}"><a href="{{ action('EventsController@getTestEvent') }}">Тестирование событий</a></li>
+    <li class="{{ Ekko::isActiveMatch('test-event') }}"><a href="{{ action('EventsController@getTestEvent') }}">Тестирование событий</a></li>
     {{--<li role="separator" class="divider"></li>--}}
     {{--<li><a href="#">Separated link</a></li>--}}
 {{--</ul>--}}
