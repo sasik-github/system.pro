b8de08e93ddd68a1b6dd334aeed02bffac6b77ae
Alexandr Spesivtsev <dart_sas@mail.ru>
2016-04-05 17:14:38 +0700

    events chart design

---
+++ resources/views/parentProfile/_eventsChart.blade.php
@@ -0,0 +1,24 @@
+<div class="row">
+
+    <div class="col-sm-12">
+
+        <div class="row">
+            <div class="col-sm-4 col-sm-push-4">
+                <div class='input-group date' id='chart_date'>
+                    <input type='text' class="form-control" />
+                    <span class="input-group-addon">
+                        <span class="glyphicon glyphicon-calendar"></span>
+                    </span>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12 chart">
+                <canvas id="myChart" width="800" height="400"></canvas>
+            </div>
+
+            <input type="hidden" id="chart_card_number" value="{{$child->card_number}}">
+        </div>
+    </div>
+</div>
--- resources/views/parentProfile/parentProfileEvents.blade.php
+++ resources/views/parentProfile/parentProfileEvents.blade.php
@@ -12,18 +12,6 @@
     @include('parentProfile._childEvents', ['events' => $child->events()->paginate(10)])
 
 
-    <div class="chart">
-        <canvas id="myChart" width="800" height="400"></canvas>
-    </div>
-    <div>
-        <div class='input-group date' id='chart_date'>
-            <input type='text' class="form-control" />
-            <span class="input-group-addon">
-                <span class="glyphicon glyphicon-calendar"></span>
-            </span>
-        </div>
-        <input type="hidden" id="chart_card_number" value="{{$child->card_number}}">
-        {{ var_dump($child) }}
-    </div>
+     @include('parentProfile._eventsChart')
 
 @endsection
